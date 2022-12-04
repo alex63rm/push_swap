d7bff625870d844db31fe940c38f93b525878023
Alex Rodriguez <alex.rodriguez1003@gmail.com>
2022-12-04 22:06:06 +0100

    created main

---
+++ push_swap_mycomments/main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejarod <alejarod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 19:51:23 by alejarod          #+#    #+#             */
+/*   Updated: 2022/12/04 20:18:09 by alejarod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_check_parameters(char *str)
+{
+	
+}
+
+
+/* In case of error, it must display "Error" followed by a ’\n’ on the standard error.
+Errors include for example: some arguments aren’t integers, some arguments are
+bigger than an integer and/or there are duplicates */
+
+
+int	main(int argc, char **argv)
+{
+	unsigned int	i;
+	
+	if (argc != 1)
+	{
+		ft_check_parameters(*argv[i]);
+		
+	}
+	return (0);
+}
