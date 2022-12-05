1e1335e28219bd5c9fce61ce7266de3b72e809ff
Alex Rodriguez <alex.rodriguez1003@gmail.com>
2022-12-05 20:16:16 +0100

    ft_check_param

--- push_swap_mycomments/main.c
+++ push_swap_mycomments/main.c
@@ -6,28 +6,63 @@
 /*   By: alejarod <alejarod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 19:51:23 by alejarod          #+#    #+#             */
-/*   Updated: 2022/12/04 20:18:09 by alejarod         ###   ########.fr       */
+/*   Updated: 2022/12/05 17:57:49 by alejarod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+
+
 void	ft_check_parameters(char *str)
 {
+	unsigned int i;
+
+
+	i = 0;	
+	while (str[i])
+	{
+		if (str[i] == '\"') // if we find "" we have to scroll through this string
+			// ft_separate_arg (like split)
+					// si hay algo que no es número, salvo espacios o más de un signo
+			printf("there is \", ft_stplit\n");
+	i++;
+	}
+			// check if there are just numbers, signs 
+		//ft_atoi()
+		printf("ft_atoi\n");
+	printf("argument is: %s\n", str);
+	
+	// error if duplicates
+
+	// error if n > max_int or lower than min_int
+	
+	// error if not an int
+
 	
 }
 
 
-/* In case of error, it must display "Error" followed by a ’\n’ on the standard error.
-Errors include for example: some arguments aren’t integers, some arguments are
-bigger than an integer and/or there are duplicates */
+
 
 
 int	main(int argc, char **argv)
 {
-	unsigned int	i;
+	int	i;
 	
-	if (argc != 1)
+	i = 1;
+	// if just ./a.out or just one argument, return nothing
+	if (argc > 2)
 	{
-		ft_check_parameters(*argv[i]);
+		while (i < argc)
+		{
+			ft_check_parameters(argv[i]);
+			i++;
+		}
+		// ft_save_ints;
+		/* 		Debes escribir un programa llamado push_swap que recibirá como argumento el
+		stack a con el formato de una lista de enteros. El primer argumento debe ser el que
+		esté encima del stack (cuidado con el orden). */
 		
 	}
 	return (0);
